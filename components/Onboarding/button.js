9e285df10af04ef77419058496adeb54b8913875
d-huliu <huliud9vid@gmail.com>
2024-05-25 13:12:12 -0400

    Admin upload feature progress and git push oriogin feature-branch

---
+++ components/Onboarding/button.js
@@ -0,0 +1,52 @@
+// import Border from '../UI/Border';
+import LoaderAnime from '../UI/LoaderAnime';
+import Shadow from '../UI/Shadow';
+
+const OnboardingButton = ({
+  children,
+  isLoading,
+  onClick,
+  disabled,
+  theme,
+  extraClasses = 'px-s2',
+}) => {
+  return disabled ? (
+    <button
+      className={`transition-300 w-full cursor-not-allowed rounded-full bg-gray-1 pt-s1.5 pb-s1 text-lg font-medium text-black ${extraClasses}`}
+      onClick={null}
+      disabled
+    >
+      {children}
+    </button>
+  ) : theme === 'error' ? (
+    <button
+      className="w-full rounded-full bg-red p-3"
+      onClick={isLoading ? () => null : onClick}
+    >
+      {isLoading ? <LoaderAnime /> : children}
+    </button>
+  ) : (
+    <Shadow classes="w-full">
+      <div
+        className={`${
+          theme !== 'white' && 'gradient-1'
+        } inline-block w-full rounded-full p-[3px]`}
+      >
+        <button
+          id="opener"
+          className={`transition-300 w-full cursor-pointer rounded-full pt-s1.5 pb-s1 text-lg font-medium ${extraClasses} ${
+            theme === 'light' && 'text-black'
+          } ${theme === 'dark' && 'bg-black text-white'} ${
+            theme === 'white' &&
+            'border-[3px] border-solid border-white bg-black text-white hover:bg-white hover:text-black'
+          }`}
+          onClick={isLoading ? () => null : onClick}
+        >
+          {isLoading ? <LoaderAnime /> : children}
+        </button>
+      </div>
+    </Shadow>
+  );
+};
+
+export default OnboardingButton;
