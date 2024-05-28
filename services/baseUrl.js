7c7607a1e4eb28805c29ab33f3905ff45dcde4f8
victor <vicopem@gmail.com>
2024-05-28 17:01:15 -0400

    Base url fiox

--- services/baseUrl.js
+++ services/baseUrl.js
@@ -1,6 +1,6 @@
-// export const baseUrl =
-//   process.env.NODE_ENV === 'development'
-//     ? 'http://localhost:4000/'
-//     : 'https://api.aviewint.com/';
+export const baseUrl =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:4000/'
+    : 'https://api.aviewint.com/';
 
-export const baseUrl = 'http://[::1]:4000/';
+// export const baseUrl = 'http://[::1]:4000/';
